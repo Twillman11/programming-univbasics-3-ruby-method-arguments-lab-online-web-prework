11a4acf305d34889c9b88778933a8c88bbb9eac6
Forrest Twillman <twillmanforrest@gmail.com>
2019-11-27 02:48:35 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,16 +5,11 @@
 def  introduction(name)
   puts "Hi, my name is #{name}."
 
-  introduction("Josh")
-  introduction("Dan")
 end
 
 def introduction_with_language(name,language)
  puts  "Hi, my name is #{name} and I am learning to program in #{language}."
  
-
- introduction_with_language("Dan" , "Ember.js")
- introduction_with_language("Josh", "React")
 end
 
 

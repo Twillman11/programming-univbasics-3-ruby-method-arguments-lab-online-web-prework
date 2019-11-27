9b4b361b36394e9698b3c5c298ca16529def29f6
Forrest Twillman <twillmanforrest@gmail.com>
2019-11-27 03:34:18 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -12,7 +12,7 @@ def introduction_with_language(name,language)
  
 end
 
-def introduction_with_language_optional(name, language = 'Ruby')
+def introduction_with_language_optional(name="Edwin")
   puts "Hi, my name is #{name} and I am learning to program in #{language} ."
   
   introduction_with_language_optional("Edwin")

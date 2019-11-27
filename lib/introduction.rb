0ecc05585adb18ecd769c702bb5229d07f62016f
Forrest Twillman <twillmanforrest@gmail.com>
2019-11-27 02:56:21 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -12,7 +12,7 @@ def introduction_with_language(name,language)
  
 end
 
-def introduction_with_language_optional(name, language)
+def introduction_with_language_optional(name, language = 'Ruby')
   puts
 
 
